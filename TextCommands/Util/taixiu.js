21eb72ab19ffd2ce84d88f4774650325bd374d78
Duke <94008471+DuckMei@users.noreply.github.com>
2024-06-02 22:50:18 +0200

    Update taixiu.js

--- TextCommands/Util/taixiu.js
+++ TextCommands/Util/taixiu.js
@@ -17,10 +17,21 @@ module.exports = {
   usage: "{prefix}taixiu",
   async execute(client, message, args) {
     if (!message.member.roles.cache.find(r => r.name === "Lắc Tài Xỉu")) return client.deleteMsg(message, `${client.e.error} Bạn phải có role \`Lắc Tài Xỉu\` để có thể sử dụng lệnh này!`, 8000, "reply")
-    let r1 = Math.floor(Math.random() * emojis.length);
-    let r2 = Math.floor(Math.random() * emojis.length);
-    let r3 = Math.floor(Math.random() * emojis.length);
-    const diem = (r1 + 1) + (r2 + 1) + (r3 + 1);
+
+    function shuffle(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]
+      }
+      return array;
+    }
+
+    let shuffledEmoji = shuffle([...emojis])
+    let r1 = shuffledEmoji[0];
+    let r2 = shuffledEmoji[1];
+    let r3 = shuffledEmoji[2];
+    const diem = (emojis.indexOf(r1) + 1) + (emojis.indexOf(r2) + 1) + (emoji.indexOf(r3) + 1)
+
     let msg = await message.channel.send(`${tx} ${tx} ${tx}`)
     let msg2 = await message.channel.send("<:dm_uongtra:1141803196944236606> **Đợi xíu để lắc nè**")
     await client.sleep(2000)
