45859de9c6bff5bbfb48b7fcb56521ed1f8f71f3
Duke <94008471+DuckMei@users.noreply.github.com>
2024-06-02 07:40:02 +0200

    Update serverlist.js

--- TextCommands/Dev/serverlist.js
+++ TextCommands/Dev/serverlist.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder } = require("discord.js")
+/* const { EmbedBuilder } = require("discord.js")
 
 module.exports = {
   name: "serverlist",
@@ -25,4 +25,104 @@ module.exports = {
 
     await message.channel.send({ embeds: [embed]})
   }
-}
+} */
+
+const {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ComponentType,
+  EmbedBuilder,
+} = require("discord.js");
+
+module.exports = {
+  name: "serverlist",
+  group: "dev",
+  aliases: ["serverl", "svl"],
+  cooldown: 0,
+  description: "Hiển thị danh sách server mà client đã tham gia...",
+  usage: "{prefix}serverlist",
+  async execute(client, message, args) {
+    const pageSize = 10; // Số lượng server mỗi trang
+    let pageIndex = 0; // Trang hiện tại
+
+    // Hàm tạo nội dung cho embed
+    const createDescription = (guilds, page) => {
+      return (
+        `Tổng số server - ${guilds.length}\n\n` +
+        guilds
+          .sort((a, b) => b.memberCount - a.memberCount)
+          .slice(page * pageSize, (page + 1) * pageSize)
+          .map(
+            (r, i) =>
+              `**${page * pageSize + i + 1}** - **${r.name}** | \`${r.memberCount}\` Thành viên\nID - ${r.id}`
+          )
+          .join("\n")
+      );
+    };
+
+    const guilds = client.guilds.cache.map((guild) => guild);
+    const totalPages = Math.ceil(guilds.length / pageSize);
+
+    const createEmbed = (page) => {
+      const description = createDescription(guilds, page);
+      return new EmbedBuilder().setDescription(description);
+    };
+
+    const createButtons = (page) => {
+      const row = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+          .setCustomId("previous")
+          .setLabel("Trước")
+          .setStyle(ButtonStyle.Primary)
+          .setDisabled(page === 0),
+        new ButtonBuilder()
+          .setCustomId("next")
+          .setLabel("Sau")
+          .setStyle(ButtonStyle.Primary)
+          .setDisabled(page >= totalPages - 1)
+      );
+      return row;
+    };
+
+    const embed = createEmbed(pageIndex);
+    const row = createButtons(pageIndex);
+
+    const reply = await message.channel.send({
+      embeds: [embed],
+      components: [row],
+    });
+
+    const collector = reply.createMessageComponentCollector({
+      componentType: ComponentType.Button,
+      time: 300000,
+    });
+
+    collector.on("collect", async (interaction) => {
+      if (interaction.member.id !== message.author.id) return;
+      await interaction.deferUpdate();
+      pageIndex += interaction.customId === "previous" ? -1 : 1;
+      const newEmbed = createEmbed(pageIndex);
+      const newRow = createButtons(pageIndex);
+      await interaction.editReply({ embeds: [newEmbed], components: [newRow] });
+    });
+
+    collector.on("end", async () => {
+      const disabledRow = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+          .setCustomId("previous")
+          .setLabel("Trước")
+          .setStyle(ButtonStyle.Primary)
+          .setDisabled(true),
+        new ButtonBuilder()
+          .setCustomId("next")
+          .setLabel("Sau")
+          .setStyle(ButtonStyle.Primary)
+          .setDisabled(true)
+      );
+      await reply.edit({
+        components: [disabledRow],
+      });
+    });
+  },
+};
