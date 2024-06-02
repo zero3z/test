f1c99cab3b016d846754ec5fba4577082f2a9949
Duke <94008471+DuckMei@users.noreply.github.com>
2024-06-02 10:44:31 +0200

    Update emoji.js

--- TextCommands/Util/emoji.js
+++ TextCommands/Util/emoji.js
@@ -1,5 +1,4 @@
 const {
-  parseEmoji,
   ActionRowBuilder,
   ButtonBuilder,
   ButtonStyle,
@@ -10,51 +9,36 @@ const {
 module.exports = {
   name: "emoji",
   async execute(client, message, args) {
+    let text = args.join(" ");
+    let emojis = parseIDs(text);
+
+    if (emojis.length === 0) {
+      return client.deleteMsg(message, `${client.e.error} Vui lòng thêm emoji cần phóng to!`, 5000, "reply");
+    }
+
     let index = 0;
-    const emojiLength = args.length;
-    const emoji = args[index];
-    if (!emoji) {
-      return client.deleteMsg(
-        message,
-        `${client.e.error} Vui lòng thêm emoji cần phóng to!`,
-        5000,
-        "reply",
-      );
-    }
-    const custom = await parseEmoji(emoji);
-    if (!custom || !custom.id) {
-      return client.deleteMsg(
-        message,
-        `${client.e.error} Vui lòng chọn một emoji phù hợp!`,
-        5000,
-        "reply",
-      );
-    }
-    const embed = new EmbedBuilder()
-      .setAuthor({ name: "Big Emoji!!" })
-      .setDescription(`\`${custom.name} ${custom.id}\``)
-      .setImage(
-        `https://cdn.discordapp.com/emojis/${custom.id}.${
-          custom.animated ? "gif" : "png"
-        }`,
-      )
-      .setFooter({
-        text: `Trang ${index + 1}/${emojiLength}`,
-      });
 
-    const button1 = new ButtonBuilder()
-      .setCustomId("back_buttonEmoji")
-      .setEmoji("<:back_button:1164742324107087893>")
-      .setStyle(ButtonStyle.Secondary)
-      .setDisabled(true);
+    const createEmbed = (emoji, pageIndex) => new EmbedBuilder()
+      .setAuthor({ name: "Emoji phóng to", url: emoji.url, iconURL: message.author.displayAvatarURL() })
+      .setDescription(`\`${emoji.name} ${emoji.id}\``)
+      .setImage(emoji.url)
+      .setFooter({ text: `Trang ${pageIndex + 1}/${emojis.length}` });
 
-    const button2 = new ButtonBuilder()
-      .setCustomId("foward_buttonEmoji")
-      .setEmoji("<:forward_button:1164742350933864448>")
-      .setStyle(ButtonStyle.Secondary)
-      .setDisabled(index + 1 >= emojiLength);
+    const createButtons = (pageIndex) => new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("back_buttonEmoji")
+        .setEmoji("<:back_button:1164742324107087893>")
+        .setStyle(ButtonStyle.Secondary)
+        .setDisabled(pageIndex === 0),
+      new ButtonBuilder()
+        .setCustomId("forward_buttonEmoji")
+        .setEmoji("<:forward_button:1164742350933864448>")
+        .setStyle(ButtonStyle.Secondary)
+        .setDisabled(pageIndex + 1 >= emojis.length)
+    );
 
-    const row = new ActionRowBuilder().addComponents(button1, button2);
+    const embed = createEmbed(emojis[index], index);
+    const row = createButtons(index);
 
     const reply = await message.reply({ embeds: [embed], components: [row] });
 
@@ -63,37 +47,44 @@ module.exports = {
       time: 300000,
     });
 
-    collector
-      .on("collect", async (interaction) => {
-        if (interaction.member.id !== message.author.id) return;
-        await interaction.deferUpdate();
-        index += interaction.customId === "back_buttonEmoji" ? -1 : 1;
-        button1.setDisabled(index === 0);
-        button2.setDisabled(index + 1 >= emojiLength);
+    collector.on("collect", async (interaction) => {
+      if (interaction.member.id !== message.author.id) return;
+      await interaction.deferUpdate();
+      index += interaction.customId === "back_buttonEmoji" ? -1 : 1;
+      const newEmbed = createEmbed(emojis[index], index);
+      const newRow = createButtons(index);
+      await interaction.editReply({ embeds: [newEmbed], components: [newRow] });
+    });
 
-        const nemoji = args[index];
-        const ncustom = await parseEmoji(nemoji);
-
-        const embed1 = new EmbedBuilder()
-          .setAuthor({ name: "Big Emoji!!" })
-          .setDescription(`\`${ncustom.name} ${ncustom.id}\``)
-          .setImage(
-            `https://cdn.discordapp.com/emojis/${ncustom.id}.${
-              ncustom.animated ? "gif" : "png"
-            }`,
-          )
-          .setFooter({
-            text: `Trang ${index + 1}/${emojiLength}`
-          });
-
-        await interaction.editReply({ embeds: [embed1], components: [row] });
-      })
-      .on("end", async () => {
-        button1.setDisabled(true);
-        button2.setDisabled(true);
-        await reply.edit({
-          components: [row],
-        });
-      });
+    collector.on("end", async () => {
+      const disabledRow = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+          .setCustomId("back_buttonEmoji")
+          .setEmoji("<:back_button:1164742324107087893>")
+          .setStyle(ButtonStyle.Secondary)
+          .setDisabled(true),
+        new ButtonBuilder()
+          .setCustomId("forward_buttonEmoji")
+          .setEmoji("<:forward_button:1164742350933864448>")
+          .setStyle(ButtonStyle.Secondary)
+          .setDisabled(true)
+      );
+      await reply.edit({ components: [disabledRow] });
+    });
   },
 };
+
+function parseIDs(text) {
+  const emojis = [];
+  const parsedEmojis = text.match(/<[as]?:[a-z0-9_ ]+:[0-9]+>/gi);
+
+  parsedEmojis.forEach(emoji => {
+    const id = emoji.match(/:[0-9]+>/gi)[0].slice(1, -1);
+    const name = emoji.match(/:[a-z0-9_]+:/gi)[0].slice(1, -1);
+    const gif = emoji.startsWith('<a:');
+    const url = `https://cdn.discordapp.com/emojis/${id}.${gif ? "gif" : "png"}`;
+    emojis.push({ name, id, gif, url });
+  });
+
+  return emojis;
+}
